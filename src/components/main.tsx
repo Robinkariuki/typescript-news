2e618bd99801c77ad64e363ca08e79fa92637128
unknown <robinkaris7@gmail.com>
2023-03-01 17:06:37 +0300

    made changes

---
+++ src/components/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Grid from './grid'
+import Header from './header'
+
+const Main = () => {
+  return (
+    <React.Fragment>
+    <Header/>
+    <Grid/>
+    </React.Fragment>
+  )
+}
+
+export default Main
