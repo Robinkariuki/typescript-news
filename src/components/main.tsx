743155964f66fc3f22f8c209eb1d11bb9dc3ce00
unknown <robinkaris7@gmail.com>
2023-03-02 14:24:58 +0300

    made search bar component

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Grid from './grid'
 import Header from './header'
 
-const Main = () => {
+const Main:React.FC = () => {
   return (
     <React.Fragment>
     <Header/>
