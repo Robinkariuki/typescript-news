ece3a294ab6d80d88460d01d0784d2ba34072e53
unknown <robinkaris7@gmail.com>
2023-03-10 16:51:18 +0300

    made chnages

--- src/hooks/helper.tsx
+++ src/hooks/helper.tsx
@@ -8,22 +8,42 @@ export function useNewsContextValue(): NewsContextData{
         
     const [news,setNews] = useState<newsData[]>([])
     const [isLoading, setIsLoading] = useState(false);
+    const [length, setLength] = useState(0)
+    const [page, setPage] = useState(1)
+    const [isLastPage,setLastPage] = useState(true)
+    
 
     const fetchPosts = useCallback(()=>{
+    
         setIsLoading(true)
-        fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=e1009a570111462d9295724c7b573134`)
+        fetch(`https://newsapi.org/v2/top-headlines?country=us&page=${page}&pageSize=10&apiKey=e1009a570111462d9295724c7b573134`)
         .then(response =>  response.json())
         .then((fetchedNews)=>{
-            setNews(fetchedNews.articles)
+            setNews([...news,...fetchedNews.articles])
+            setLength(fetchedNews.totalResults)
+            setPage(page + 1)
+            
+            console.log(fetchedNews.totalResults)
+        
         }).finally(()=>{
             setIsLoading(false)
+            if(length === news.length){
+                setLastPage(false)
+
+            }
+           
         })
-    },[setNews]);
+    },[setNews,page,news,length]);
 
     return{
         news,
         isLoading,
-        fetchPosts
+        fetchPosts,
+        length,
+        page,
+        isLastPage
+   
+
 
     }
 
